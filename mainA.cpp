ef2bbf0b74dca68136ec6cc552476ecd4aeb0179
lawMS <50967742+lawMS@users.noreply.github.com>
2020-07-16 17:07:48 -0500

    Create mainA.cpp
    Clase principal

---
+++ mainA.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cstdlib>
+#include "Arbol.h"
+
+
+using namespace std;
+
+nodo *arbol =NULL;
+
+int main(int argc, char** argv) {
+	int dato;
+	do{
+		cout<<"Insertar un nuevo nodo al arbol"<<endl;
+		cout<<"digite el numero"<<endl;
+		cin>>dato;
+		
+	}while(dato != 2);
+	
+	
+	
+	//system("PAUSE");
+    //return EXIT_SUCCESS;
+}
